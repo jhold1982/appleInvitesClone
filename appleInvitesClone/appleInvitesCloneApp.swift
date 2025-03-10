b9fbc27c80b4c3e80e68334d9322bd39f6fbe993
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-03-10 13:57:18 -0500

    Initial Commit

---
+++ appleInvitesClone/appleInvitesCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleInvitesCloneApp.swift
+//  appleInvitesClone
+//
+//  Created by Justin Hold on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct appleInvitesCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
